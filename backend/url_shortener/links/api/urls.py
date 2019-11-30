e5c1cc79d1038116b0ca8f5a3efc3099272f67b3
Taras Venger <taras-venger@users.noreply.github.com>
2019-11-30 17:20:12 +0200

    Redirection implemented

--- backend/url_shortener/links/api/urls.py
+++ backend/url_shortener/links/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LinksListView, LinksCreateView
+from .views import LinksListView, LinksCreateView, redirect_view
 
 urlpatterns = [
     path('', LinksListView.as_view()),
     path('create/', LinksCreateView.as_view()),
+    path('<slug:short_url>/', redirect_view)
 ]
--- backend/url_shortener/links/api/views.py
+++ backend/url_shortener/links/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import ListAPIView, CreateAPIView
 from links.models import Links
 from .serializers import LinksSerializer
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 class LinksListView(ListAPIView):
@@ -11,3 +13,8 @@ class LinksListView(ListAPIView):
 class LinksCreateView(CreateAPIView):
     queryset = Links.objects.all()
     serializer_class = LinksSerializer
+
+
+def redirect_view(request, short_url=None, *args, **kwargs):
+    obj = get_object_or_404(Links, short_url=short_url)
+    return HttpResponseRedirect(obj.original_url)
