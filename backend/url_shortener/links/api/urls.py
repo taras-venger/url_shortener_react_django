18e8cd2c7dffaa6f2d8b85f85116927ba7702b2f
Taras Venger <taras-venger@users.noreply.github.com>
2019-12-01 15:02:04 +0200

    Added 'delete_expired' view

--- backend/url_shortener/links/api/urls.py
+++ backend/url_shortener/links/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import LinksListView, LinksCreateView, redirect_view
+from .views import (LinksListView,
+                    LinksCreateView,
+                    redirect_view,
+                    delete_expired)
 
 urlpatterns = [
     path('', LinksListView.as_view()),
     path('create/', LinksCreateView.as_view()),
+    path('delete_expired/', delete_expired),
     path('<slug:short_url>/', redirect_view)
 ]
--- backend/url_shortener/links/api/views.py
+++ backend/url_shortener/links/api/views.py
@@ -3,6 +3,7 @@ from links.models import Links
 from .serializers import LinksSerializer
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
+from datetime import datetime, date
 
 
 class LinksListView(ListAPIView):
@@ -18,3 +19,12 @@ class LinksCreateView(CreateAPIView):
 def redirect_view(request, short_url=None, *args, **kwargs):
     obj = get_object_or_404(Links, short_url=short_url)
     return HttpResponseRedirect(obj.original_url)
+
+
+def delete_expired(request):
+    queryset = Links.objects.all()
+    today = date.today()
+    for q in queryset:
+        if (date.today() - q.created_on).days > q.lifetime_days:
+            q.delete()
+    return HttpResponse('remove expired links')
