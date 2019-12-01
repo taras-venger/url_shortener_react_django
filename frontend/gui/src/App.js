bc595c543733e075e9e4264c67277432ec0170c8
Taras Venger <taras-venger@users.noreply.github.com>
2019-12-01 16:05:23 +0200

    clear expired urls on component did mount

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import CustomForm from './components/Form';
+import CustomForm from './components/Form/Form';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/api/delete_expired/')
+      .then(res => console.log('deleted'))
+      .catch(err => console.log(err.message));
+  }, []);
+
   return (
     <div className='App'>
+      <h1>Gentle URL Shortener </h1>
       <CustomForm />
     </div>
   );
